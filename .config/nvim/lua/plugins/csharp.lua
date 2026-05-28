52b2778a896adf9fe73c461a631d731a988e6b20
Chris Van Treese <chris.vantreese@dcsg.com>
2026-05-28 09:37:01 -0500

    update csharp config

--- .config/nvim/lua/plugins/csharp.lua
+++ .config/nvim/lua/plugins/csharp.lua
@@ -1,7 +1,20 @@
+-- ~/.config/nvim/lua/plugins/roslyn.lua
 return {
   {
     "seblyng/roslyn.nvim",
     ft = "cs",
-    opts = {},
+    opts = {
+      config = {
+        settings = {
+          ["csharp|background_analysis"] = {
+            ["background_analysis.dotnet_compiler_diagnostics_scope"] = "fullSolution",
+            ["background_analysis.dotnet_analyzer_diagnostics_scope"] = "fullSolution",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("roslyn").setup(opts)
+    end,
   },
 }
